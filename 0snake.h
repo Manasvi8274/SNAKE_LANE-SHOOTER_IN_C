6b7515c20d88e9a4bfc740442e0d660c3ab58108
Manasvi8274 <Manasvi1238274@gmail.com>
2024-08-23 00:59:12 +0530

    commit

---
+++ 0snake.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <conio.h>
+#include <time.h>
+
+// int height = 20, width = 30, x, y, fx, fy, flag, gameover, score;
+
+// int tailx[100], taily[100];
+// int piece = 0;
+
+void snake_makelogic();
+void snake_input();
+void sbake_setup();
+void snake_draw();
+void snake_draw_gameover();
