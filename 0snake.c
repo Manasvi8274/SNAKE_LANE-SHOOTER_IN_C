18475ee32b3ccb072d86831a3ede806907e15908
Manasvi8274 <Manasvi1238274@gmail.com>
2024-08-23 11:27:17 +0530

    commit

--- 0snake.c
+++ 0snake.c
@@ -64,3 +64,69 @@ void snake_draw()
         printf("\n");
     }
 }
+void snake_draw_gameover()
+{
+    int i, j;
+    for (i = 0; i <= height; i++)
+    {
+        for (j = 0; j <= width; j++)
+        {
+            if (i == 0 || i == height || j == 0 || j == width)
+            {
+                printf("%c", 178);
+            }
+            else
+            {
+                // if(i == 9 && j==6)
+                // 	printf("G");
+                // if (i == 9 && j == 7)
+                // 	printf("A");
+                // if (i == 9 && j == 8)
+                // 	printf("M");
+                // if (i == 9 && j == 9)
+                // 	printf("E");
+                // if (i == 9 && j == 10)
+                // 	printf("O");
+                // if (i == 9 && j == 11)
+                // 	printf("V");
+                // if (i == 9 && j == 12)
+                // 	printf("E");
+                // if (i == 9 && j == 13)
+                // 	printf("R");
+                if (i == 9 && (j == 11))
+                {
+                    printf("G");
+                    // Sleep(50);
+                    printf("A");
+                    // Sleep(50);
+                    printf("M");
+                    // Sleep(50);
+                    printf("E");
+                    // Sleep(50);
+                    printf("O");
+                    // Sleep(50);
+                    printf("V");
+                    // Sleep(50);
+                    printf("E");
+                    // Sleep(50);
+                    printf("R");
+                    // Sleep(50);
+                }
+                else if (i == 9 && (j <= 18 && j >= 11))
+                    continue;
+
+                else
+                    printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    Beep(1000, 20);
+    Beep(900, 20);
+    Beep(800, 20);
+    Beep(700, 20);
+    Beep(600, 20);
+    Beep(500, 20);
+    Beep(400, 20);
+    Beep(300, 20);
+}
